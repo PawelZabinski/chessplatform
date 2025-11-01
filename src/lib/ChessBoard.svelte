c666874b5ec739975b7a322c96194dbd16bb7c59
Pawel Zabinski <pawelpow@icloud.com>
2025-11-01 21:56:59 +0000

    extracted state + added random piece removal

--- src/lib/ChessBoard.svelte
+++ src/lib/ChessBoard.svelte
@@ -1,21 +1,30 @@
 <script lang="ts">
+    import { onMount } from 'svelte';
 	import { Chess, type GameOverEvent, type MoveEvent } from 'svelte-chess';
 	import { Engine } from '$lib/engine';
-	import { moves } from '$lib/stores/chess';
+	import { moves, chessState, handlePieceRemovalFunction } from '$lib/stores/chess';
 
-	const engine = new Engine({ depth: 1, randomMoveProbability: 0.4 });
+    let chessboard;
+	const engine = new Engine({ depth: 1, randomMoveProbability: 0.5 });
 
 	function handleMove(event: MoveEvent) {
-		moves.add(event.detail);
+		chessState.addMove(event.detail);
 	}
 
 	function handleGameOver(event: GameOverEvent) {
 		console.log('Game over');
 		console.log(event.detail);
 	}
+
+    function removeRandomPiece() {
+        chessState.removeRandomPiece(newFen => chessboard.load(newFen))
+    }
+
+    onMount(() => handlePieceRemovalFunction.set(removeRandomPiece))
 </script>
 
 <Chess 
+    bind:this={chessboard}
 	on:move={handleMove}
 	on:gameOver={handleGameOver}
 	engine={engine}
--- src/lib/stores/chess.ts
+++ src/lib/stores/chess.ts
@@ -1,24 +1,69 @@
-import { writable } from 'svelte/store';
+import { get, writable } from 'svelte/store';
 import type { Move } from 'svelte-chess';
+import { Chess } from "chess.js";
 
 type MovesStore = {
 	subscribe: ReturnType<typeof writable<Move[]>>['subscribe'];
 	add: (move: Move) => void;
-	reset: () => void;
 };
 
 function createMovesStore(): MovesStore {
-	const { subscribe, set, update } = writable<Move[]>([]);
+	const { subscribe, update } = writable<Move[]>([]);
 
 	return {
 		subscribe,
 		add(move) {
 			update((moves) => [...moves, move]);
-		},
-		reset() {
-			set([]);
 		}
 	};
 }
 
 export const moves = createMovesStore();
+
+function createChessStateStore() {
+	const { subscribe, update, set } = writable("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+
+	return {
+		subscribe,
+		addMove(move) {
+			moves.add(move);
+			set(move.after);
+		},
+		removeRandomPiece: (handleNewFen = (newFen) => {}) => update((fen: string) => {
+			// Removes a random piece (except the king itself...)
+			const chess = new Chess(fen)
+			// get all pieces, filter to white ones
+			const pieces = [];
+			for (let file = 0; file < 8; file++) {
+			for (let rank = 0; rank < 8; rank++) {
+				const square = "abcdefgh"[file] + (rank + 1);
+				const piece = chess.get(square);
+				if (piece && piece.color === "w" && piece.type !== "k") pieces.push({ square, piece });
+			}
+			}
+
+			// remove one at random
+			if (pieces.length > 0) {
+				const toRemove = pieces[Math.floor(Math.random() * pieces.length)];
+				chess.remove(toRemove.square);
+			}
+
+			// generate new FEN
+			const newFen = chess.fen();
+			handleNewFen(newFen);
+			return newFen;
+		})
+	};
+}
+
+export const chessState = createChessStateStore();
+
+// This function should only be set by Chessboard.svelte and should only be used to remove random piece when player hits spike
+export const handlePieceRemovalFunction = (() => {
+	const {subscribe, set, update} = writable(() => {})
+
+	return {
+		set,
+		execute: () => subscribe(f => f())()
+	};
+})()
